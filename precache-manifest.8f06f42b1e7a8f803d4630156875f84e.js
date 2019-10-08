0265fdf2bec5ce7c7cce9d6dfc4f6c532537d590
andrey <munkk@yandex.ru>
2019-10-08 13:40:58 +0300

    Updates

---
+++ precache-manifest.8f06f42b1e7a8f803d4630156875f84e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaa959e58020aed6f6568054bdd9262c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2042e35d7e6d3968cf1f",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "13d53c31122a4fe510a2",
+    "url": "/static/js/2.b09676ea.chunk.js"
+  },
+  {
+    "revision": "2042e35d7e6d3968cf1f",
+    "url": "/static/js/main.2f4321ef.chunk.js"
+  },
+  {
+    "revision": "51d41d0445906ab1ba07",
+    "url": "/static/js/runtime-main.3ebd819c.js"
+  }
+]);
