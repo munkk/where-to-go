810c63df5048c14567f43edc74e1a2f3827a649f
andrey <munkk@yandex.ru>
2019-10-08 13:27:30 +0300

    Updates

---
+++ precache-manifest.cfb0aec6cf2911b4289bcf6d0c626400.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df6a711126f675a7b2ba74c236d1090b",
+    "url": "/where-to-go/index.html"
+  },
+  {
+    "revision": "c5fb913eb247e5bf7cc6",
+    "url": "/where-to-go/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "13d53c31122a4fe510a2",
+    "url": "/where-to-go/static/js/2.b09676ea.chunk.js"
+  },
+  {
+    "revision": "c5fb913eb247e5bf7cc6",
+    "url": "/where-to-go/static/js/main.81705828.chunk.js"
+  },
+  {
+    "revision": "992ae5c18531fceb5628",
+    "url": "/where-to-go/static/js/runtime-main.48f48c56.js"
+  }
+]);
